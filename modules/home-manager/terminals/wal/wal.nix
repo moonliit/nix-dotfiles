2fe1b58f0b724720868e781fa808b9b0dad4f9c6
moonliit <d4rkmoon0503@gmail.com>
2025-08-24 00:54:30 -0500

    fuck

--- modules/home-manager/terminals/wal/wal.nix
+++ modules/home-manager/terminals/wal/wal.nix
@@ -1,10 +1,33 @@
 { config, pkgs, ... }:
 
 let
-  colors = "~/Dotfiles/modules/home-manager/terminals/wal"
+  colors = builtins.fromJSON (builtins.readFile ./colors.json);
+
+  # Generate ANSI escape sequences
+  sequences = ''
+    # Foreground
+    \u001b]10;${colors.special.foreground}\a
+    # Background
+    \u001b]11;${colors.special.background}\a
+    # Cursor
+    \u001b]12;${colors.special.cursor}\a
+  '' + (builtins.concatStringsSep "\n" (
+    builtins.attrValues (builtins.mapAttrs (name: value:
+      let index = builtins.fromJSON (builtins.toJSON (builtins.substring 5 (builtins.stringLength name) name)); in
+      "\u001b]4;${toString index};${value}\a"
+    ) colors.colors)
+  ));
+
 in
 {
+  # Write sequences to a file inside /etc
+  environment.etc."wal-sequences".text = sequences;
+
+  # Apply sequences at shell init
   programs.zsh.initExtra = ''
-    (cat ${colors} &)
+    if [ -f /etc/wal-sequences ]; then
+      cat /etc/wal-sequences
+    fi
   '';
 }
+
--- modules/home-manager/terminals/wallust/wallust.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  programs.wallust = {
-    enable = true;
-    settings = pkgs.lib.importTOML ./wallust.toml;
-  };
-}
